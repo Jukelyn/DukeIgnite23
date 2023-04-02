98ca92c23bba0e8eecfc65d6cc23f63bc9ece9f9
mahmed6 <mahmed6@ncsu.edu>
2023-04-02 00:39:52 -0400

    Updated README

---  Program Files/main.py
+++  Program Files/main.py
@@ -1,6 +1,13 @@
 import os, sys
 import openai
-from dotenv import load_dotenv # Access things from .env file
 
+from dotenv import load_dotenv # Access things from .env file
 load_dotenv()
 openai.api_key = os.getenv('GPT-TOKEN')
+
+# GPT TIME BABYYYYY WOOOOOOO
+data_path = "../data"
+
+"""
+1. GET INFO FROM TXT FILES
+"""
